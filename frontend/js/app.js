731943c8491379f88425aef9de97dd45bd4b6969
Jbrady021 <Jbrady021@gmail.com>
2022-04-12 15:55:28 -0600

    dapp

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -70,6 +70,7 @@ const updateConnectStatus = async () => {
     notConnected.classList.remove('show-not-connected');
     notConnected.classList.add('hidden');
     // SHOW SPINNER
+    spinner.classList.remove('hidden');
     window.contract = new web3.eth.Contract(abi, contractAddress);
     loadInfo();
   } else {
